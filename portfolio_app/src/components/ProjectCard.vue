6e9fc9f7003f34f51aa5f32c21ba068f3f465d5a
gmenke54 <menke.gw@gmail.com>
2022-02-19 08:32:51 -0800

    removed vue-flip plugin

--- portfolio_app/src/components/ProjectCard.vue
+++ portfolio_app/src/components/ProjectCard.vue
@@ -1,30 +1,26 @@
 <template>
   <div class="cont">
     <div class="line"></div>
-    <vue-flip active-click width="45vw" height="30vw" >
-      <template v-slot:front class="picture-cont">
-        <img :src="project.images[0]" alt="Project Image">
-      </template>
-      <template v-slot:back>
-        <div class="bullets-cont">
-          <div class="project-name">{{project.name}}</div>
-          <div class="project-description">{{project.description}}</div>
-          <div v-for="bullet in project.bullets" :key="bullet.index">-{{bullet}}</div>
-        </div>
-      </template>
-    </vue-flip>
+    <div class="project-name">{{project.name}}</div>
+    <div>
+      <img :src="project.images[0]" alt="Project Image">
+    </div>
+    <div>
+      <div class="bullets-cont">
+        <div class="project-description">{{project.description}}</div>
+        <div v-for="bullet in project.bullets" :key="bullet.index">-{{bullet}}</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import VueFlip from 'vue-flip';
 export default {
   name: 'TechCard',
   props: {
     project: Object
   },
   components: {
-    'vue-flip': VueFlip
   },
    data:() => ({
 
@@ -40,30 +36,19 @@ export default {
   margin-top: 15px;
   margin-bottom: 15px;
 }
-.flex-row{
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-}
-.flip-container{
-  margin: 0 auto;
-  transition: all 0.5s;
-  cursor: pointer
-}
-.flip-container:hover{
-  transform: scale(1.04);
-}
-.back{
-  margin: auto 0;
-  height: 30vw;
-}
 .bullets-cont{
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   width: 100%;
-  height: 100%
+  height: 100%;
+  background-color: white;
+  width: 50vw;
+  margin: 0 auto;
+  border-radius: 10px;
 }
-
+img{
+  width: 50vw;
+}
 </style>
